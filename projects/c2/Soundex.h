bc0df5be97beec51e62b4a0855bf311f224f3571
Mark Sanders <masander@capgemini.com>
2015-04-14 15:39:59 +0100

    Refactored Soundex class

--- projects/c2/Soundex.h
+++ projects/c2/Soundex.h
@@ -4,11 +4,31 @@ class Soundex
 {
 public:
     std::string encode(const std::string& word) const {
-        return zeroPad(word);
+        return zeroPad(head(word) + encodeDigits(word));
     }
 
 private:
+
+    static const size_t MAX_CODE_LENGTH { 4 };
+
+    std::string head(const std::string& word) const {
+        return word.substr(0, 1);
+    }
+
+    std::string encodeDigits(const std::string& word) const {
+        if (word.length() > 1) {
+            return encodeDigit();
+        } else {
+            return "";
+        }
+    }
+
+    std::string encodeDigit() const {
+        return "1";
+    }
+
     std::string zeroPad(const std::string& word) const {
-        return word + "000";
+        auto zerosNeeded = MAX_CODE_LENGTH - word.length();
+        return word + std::string(zerosNeeded, '0');
     }
 };
