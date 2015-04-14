2379931627de5a9d1e5df1efac88397d92e6f7e3
Mark Sanders <masander@capgemini.com>
2015-04-14 15:04:20 +0100

    Add test fixture and zero pad test

--- projects/c2/SoundexTest.cpp
+++ projects/c2/SoundexTest.cpp
@@ -1,9 +1,38 @@
-class Soundex {
+#include <string>
 
+class Soundex 
+{
+public:
+    std::string encode(const std::string& word) const {
+        return zeroPad(word);
+    }
+
+private:
+    std::string zeroPad(const std::string& word) const {
+        return word + "000";
+    }
 };
 
 #include "gmock/gmock.h"
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
+using ::testing::Eq;
+class SoundexEncoding: public testing::Test
 {
+public:
     Soundex soundex;
+};
+
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
+{
+    auto encoded = soundex.encode("A");
+
+    ASSERT_THAT(encoded, Eq("A000"));
+}
+
+
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigist)
+{
+    auto encoded = soundex.encode("I");
+
+    ASSERT_THAT(encoded, Eq("I000"));
 }
