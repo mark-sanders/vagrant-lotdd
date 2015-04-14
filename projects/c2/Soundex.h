2cdcc814c347173fb8ff5525cc6897f3fa3cb4f2
Mark Sanders <masander@capgemini.com>
2015-04-14 15:10:11 +0100

    Refactor tests and split Soundex class into its own header

---
+++ projects/c2/Soundex.h
@@ -0,0 +1,14 @@
+#include <string>
+
+class Soundex 
+{
+public:
+    std::string encode(const std::string& word) const {
+        return zeroPad(word);
+    }
+
+private:
+    std::string zeroPad(const std::string& word) const {
+        return word + "000";
+    }
+};
--- projects/c2/SoundexTest.cpp
+++ projects/c2/SoundexTest.cpp
@@ -1,22 +1,8 @@
-#include <string>
-
-class Soundex 
-{
-public:
-    std::string encode(const std::string& word) const {
-        return zeroPad(word);
-    }
-
-private:
-    std::string zeroPad(const std::string& word) const {
-        return word + "000";
-    }
-};
-
 #include "gmock/gmock.h"
+#include "Soundex.h"
+
 using ::testing::Eq;
-class SoundexEncoding: public testing::Test
-{
+class SoundexEncoding: public testing::Test {
 public:
     Soundex soundex;
 };
@@ -24,15 +10,11 @@ public:
 
 TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
 {
-    auto encoded = soundex.encode("A");
-
-    ASSERT_THAT(encoded, Eq("A000"));
+    ASSERT_THAT(soundex.encode("A"), Eq("A000"));
 }
 
 
 TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigist)
 {
-    auto encoded = soundex.encode("I");
-
-    ASSERT_THAT(encoded, Eq("I000"));
+    ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
