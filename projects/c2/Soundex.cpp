7f5606f04b5494b86ea6613fed95bf5deac7804e
Mark Sanders <masander@capgemini.com>
2015-04-16 10:46:29 +0100

    Move character to digit map to Soundex.cpp

---
+++ projects/c2/Soundex.cpp
@@ -0,0 +1,17 @@
+#include "Soundex.h"
+
+//  Replace consonants with digits as follows (after the first letter):
+//  - b, f, p, v => 1
+//  - c, g, j, k, q, s, x, z => 2
+//  - d, t       => 3
+//  - l          => 4
+//  - m, n       => 5
+//  - r          => 6
+const std::map<char, const std::string> Soundex::CHAR_TO_DIGIT_MAP {
+    { 'b', "1" }, { 'f', "1" }, { 'p', "1" }, { 'v', "1" },
+    { 'c', "2" }, { 'g', "2" }, { 'j', "2" }, { 'k', "2" }, { 'q', "2" }, { 's', "2" }, { 'x', "2" }, { 'z', "2" },
+    { 'd', "3" }, { 't', "3" },
+    { 'l', "4" },
+    { 'm', "5" }, { 'n', "5" },
+    { 'r', "6" },
+};
--- projects/c2/Soundex.h
+++ projects/c2/Soundex.h
@@ -1,4 +1,5 @@
 #include <string>
+#include <map>
 
 class Soundex 
 {
@@ -8,8 +9,9 @@ public:
     }
 
 private:
-
-    static const size_t MAX_CODE_LENGTH { 4 };
+    static const size_t MAX_ENCODED_DIGITS { 3 };
+    static const size_t MAX_CODE_LENGTH { 1 + MAX_ENCODED_DIGITS };
+    static const std::map<char, const std::string> CHAR_TO_DIGIT_MAP;
 
     std::string head(const std::string& word) const {
         return word.substr(0, 1);
@@ -18,32 +20,17 @@ private:
     std::string encodeDigits(const std::string& word) const {
         std::string digits;
 
-        for (auto iter = word.begin() + 1; iter != word.end() && digits.length() < 3; iter++) {
+        for (auto iter = word.begin() + 1; iter != word.end() && digits.length() < MAX_ENCODED_DIGITS; iter++) {
             digits += encodeDigit(*iter);
         }
 
         return digits;
     }
 
-    //  Replace consonants with digits as follows (after the first letter):
-    //  - b, f, p, v => 1
-    //  - c, g, j, k, q, s, x, z => 2
-    //  - d, t       => 3
-    //  - l          => 4
-    //  - m, n       => 5
-    //  - r          => 6
-    const std::map<char, const std::string> charToDigitMap = {
-        { 'b', "1" }, { 'f', "1" }, { 'p', "1" }, { 'v', "1" },
-        { 'c', "2" }, { 'g', "2" }, { 'j', "2" }, { 'k', "2" }, { 'q', "2" }, { 's', "2" }, { 'x', "2" }, { 'z', "2" },
-        { 'd', "3" }, { 't', "3" },
-        { 'l', "4" },
-        { 'm', "5" }, { 'n', "5" },
-        { 'r', "6" },
-    };
-
     std::string encodeDigit(char letter) const {
-        auto it = charToDigitMap.find(letter);
-        if (it == charToDigitMap.end()) {
+        auto it = CHAR_TO_DIGIT_MAP.find(letter);
+
+        if (it == CHAR_TO_DIGIT_MAP.end()) {
             return "";
         } else {
             return it->second;
